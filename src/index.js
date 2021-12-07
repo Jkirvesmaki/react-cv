287da0827febf1a908d542bdece51a8767965a31
Jkirvesmaki <63333403+Jkirvesmaki@users.noreply.github.com>
2021-12-07 15:33:47 +0200

    fixed a bug with the font, removed index.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
